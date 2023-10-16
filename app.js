e70f650bd137b1ab01db03a78013b1ebcf6dc5b1
shubh532 <shubhammamahulkar2000>
2023-10-16 17:51:34 +0530

    Defining Users

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const mongoose = require("mongoose")
-// const mongoConnect = require("./util/database").mongoConnect
-// const User = require("./models/user")
+const User = require("./models/user")
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -19,27 +18,39 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById("652b918f32fe10313152f1ae")
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
+app.use((req, res, next) => {
+  User.findById("652d292c58710430545ebf49")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 
-// });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+console.log("Connecting>>>>>>")
 mongoose.connect('mongodb+srv://shubhammahulkar2000:2BU10ziQ7SKKR0nC@cluster0.qrqxscz.mongodb.net/Shop?retryWrites=true&w=majority')
-.then(result=>{
-  app.listen(3000)
-})
-.catch(err=>{
-  console.log(err)
-})
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: "Shubham Mahulkar",
+          email: "shubhammahulkar2000@gmail.com",
+          cart: { items: [] }
+        })
+        user.save()
+      }
+    })
+    app.listen(3000)
+    console.log("<<<<<<<<<<<<Connected")
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 
--- models/user.js
+++ models/user.js
@@ -1,3 +1,28 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const userScherma = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [
+            {
+                productId: { type: Schema.Types.ObjectId, required: true },
+                quantity: { type: Number, required: true }
+            }
+        ]
+    }
+})
+
+module.exports = mongoose.model("User", userScherma)
+
 // const getDb = require("../util/database").getDb;
 // const mongoDB = require("mongodb")
 // const ObjectId = mongoDB.ObjectId
