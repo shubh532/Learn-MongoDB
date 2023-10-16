d01372ef9f5b12283e960ef08aa9bf5346a0facb
shubh532 <shubhammamahulkar2000>
2023-10-16 17:31:13 +0530

    connenting Mongoose & Adding & fetching Product

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const mongoose = require("mongoose")
 // const mongoConnect = require("./util/database").mongoConnect
-const User = require("./models/user")
+// const User = require("./models/user")
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -19,22 +19,22 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById("652b918f32fe10313152f1ae")
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
+// app.use((req, res, next) => {
+//   User.findById("652b918f32fe10313152f1ae")
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
 
-});
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://shubhammahulkar2000:2BU10ziQ7SKKR0nC@cluster0.qrqxscz.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://shubhammahulkar2000:2BU10ziQ7SKKR0nC@cluster0.qrqxscz.mongodb.net/Shop?retryWrites=true&w=majority')
 .then(result=>{
   app.listen(3000)
 })
--- models/product.js
+++ models/product.js
@@ -1,70 +1,25 @@
-// const getDb = require("../util/database").getDb;
-// const mongodb = require("mongodb")
+const mongoose = require("mongoose")
 
+const Schema = mongoose.Schema
 
-// class Product {
-//   constructor(title, price, description, imageUrl, id, userID ) {
-//     this.title = title;
-//     this.price = price;
-//     this.description = description;
-//     this.imageUrl = imageUrl;
-//     this._id = id;
-//     this.userID = userID;
-//   }
-//   save() {
-//     const db = getDb();
-//     let dbOp;
-//     if (this._id) {
-//       dbOp = db
-//         .collection("Products")
-//         .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this })
-//     }
-//     else {
-//       dbOp = db.collection("Products").insertOne(this)
-//     }
-//     return dbOp
-//       .then(result => {
-//         console.log(result, ">>>>>>>>>>product")
-//       })
-//       .catch(err => {
-//         console.log(err, "from product")
-//       })
-//   }
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+})
 
-//   static fetchAll() {
-//     const db = getDb();
-//     return db.collection("Products").find().toArray()
-//       .then(product => {
-//         console.log("FetchAll")
-//         return product
-//       })
-//       .catch(err => {
-//         console.log(err, "fetchall err")
-//       })
-//   }
+module.exports = mongoose.model("Product", productSchema)
 
-//   static findById(prodId) {
-//     const db = getDb();
-//     return db.collection("Products").find({ _id: new mongodb.ObjectId(prodId) }).next()
-//       .then(product => {
-//         console.log("findById")
-//         return product
-//       })
-//       .catch(err => {
-//         console.log(err, "findById err")
-//       })
-//   }
-
-//   static deleteProduct(prodId) {
-//     const db = getDb()
-//     return db.collection("Products").deleteOne({ _id: new mongodb.ObjectId(prodId) })
-//       .then(result => {
-//         console.log(result, "Deleted")
-//       })
-//       .catch(err => {
-//         console.log(err, "delete err")
-//       })
-//   }
-// }
-
-// module.exports = Product;
